94f6e2cc3d31191c4de9691dbb99a8f6431ba84f
sarthakgarg99 <sarthakgarg99@gmail.com>
2020-05-18 18:17:17 +0530

    setup project

---
+++ catkin_ws/build/drone/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/baadalvm/ros_drone/catkin_ws/src/drone
+# Build directory: /home/baadalvm/ros_drone/catkin_ws/build/drone
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
