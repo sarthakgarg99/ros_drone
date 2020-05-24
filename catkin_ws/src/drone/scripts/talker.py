4c65d46b2e12290fdb9738508dc24f06e86276db
Sarthak Garg <sarthakgarg@Sarthaks-MacBook-Air.local>
2020-05-24 13:58:42 +0530

    added timestamp for cam data

--- catkin_ws/src/drone/scripts/talker.py
+++ catkin_ws/src/drone/scripts/talker.py
@@ -10,20 +10,20 @@ size = 64000
 
 
 def talker():
-    pub = rospy.Publisher('chatter', CompressedImage, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(50) # 10hz
-    a = ""
-    for i in range(0,size):
-        a = a + "a"
-    rospy.loginfo(sys.getsizeof(a))
+    pub = rospy.Publisher('cam_data', CompressedImage, queue_size=10)
+    rospy.init_node('drone_cam', anonymous=True)
+    # rate = rospy.Rate(50) # 10hz
+    # a = ""
+    # for i in range(0,size):
+    #     a = a + "a"
+    # rospy.loginfo(sys.getsizeof(a))
     while not rospy.is_shutdown():
         msg = CompressedImage()
         msg.header.stamp = rospy.Time.now()
         msg.format = "jpeg"
         msg.data = np.zeros((10,10)).tostring()
         pub.publish(msg)
-        rate.sleep()
+        # rate.sleep()
 
 if __name__ == '__main__':
     try:
